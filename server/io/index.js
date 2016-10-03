ddb39236701f8dddb435148d914345d56401210b
gil lawson <gewl@users.noreply.github.com>
2016-10-03 10:06:58 -0400

    Rough server sockets done

--- server/io/index.js
+++ server/io/index.js
@@ -1,6 +1,8 @@
 'use strict';
 var socketio = require('socket.io');
 var io = null;
+const User = '../db/models/user.js'
+const Channel = '../db/models/user.js'
 
 module.exports = function (server) {
 
@@ -8,8 +10,28 @@ module.exports = function (server) {
 
     io = socketio(server);
 
-    io.on('connection', function () {
-        // Now have access to socket, wowzers!
+    io.on('connection', function (socket) {
+		socket.on('passLogin', function(loginName) {
+			//TOCHANGE: hit the db and get all the channels that the user is associated with
+			//something like:
+			return User.findOne({
+				where: {
+					name: loginName
+				},
+				include: [ model: Channel ]
+			}) 
+				.then(function(loggingUser) {
+					return loggingUser.channels.forEach(channel=> {
+						//TOCHANGE: need to include reponame in db, or change
+						//how things are being stored client-side
+						socket.join(channel.repoName)		
+					})
+				})
+		})
+
+		socket.on('fileChanges', function(payload) {
+			io.to(payload.channel).emit('fileChanges', payload)
+		})
     });
 
     return io;
