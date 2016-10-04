31eb49ffcc8f4161d2ac00925adccb39cc89b107
gil lawson <gewl@users.noreply.github.com>
2016-10-04 10:07:00 -0400

    removed passport/user boilerplate

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -7,40 +7,8 @@ var db = require('../_db');
 
 module.exports = db.define('user', {
     name: {
-        type: Sequelize.STRING
-    }
-}, {
-    instanceMethods: {
-        sanitize: function () {
-            return _.omit(this.toJSON(), ['password', 'salt']);
-        },
-        correctPassword: function (candidatePassword) {
-            return this.Model.encryptPassword(candidatePassword, this.salt) === this.password;
-        }
-    },
-    classMethods: {
-        generateSalt: function () {
-            return crypto.randomBytes(16).toString('base64');
-        },
-        encryptPassword: function (plainText, salt) {
-            var hash = crypto.createHash('sha1');
-            hash.update(plainText);
-            hash.update(salt);
-            return hash.digest('hex');
-        }
-    },
-    hooks: {
-        beforeCreate: function (user) {
-            if (user.changed('password')) {
-                user.salt = user.Model.generateSalt();
-                user.password = user.Model.encryptPassword(user.password, user.salt);
-            }
-        },
-        beforeUpdate: function (user) {
-            if (user.changed('password')) {
-                user.salt = user.Model.generateSalt();
-                user.password = user.Model.encryptPassword(user.password, user.salt);
-            }
-        }
-    }
+		type: Sequelize.STRING,
+		allowNull: false,
+		unique: true
+	}
 });
